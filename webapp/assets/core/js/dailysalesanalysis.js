875831f9939219d0b2801240801f577f00da56e4
novalagung <caknopal@gmail.com>
2017-12-13 12:31:08 +0700

    no message

--- webapp/assets/core/js/dailysalesanalysis.js
+++ webapp/assets/core/js/dailysalesanalysis.js
@@ -368,7 +368,7 @@ dsa.renderChartDailySalesAnalysis = function (data) {
             }, {
                 name: 'Aug Actual',
                 field: 'actualAugust',
-                color: '#1b924d',
+                color: '#106d37',
             }]
 
             var cumulativeSeptember = _.last(data.map(function (d) {
@@ -422,7 +422,7 @@ dsa.renderChartDailySalesAnalysis = function (data) {
             }, {
                 name: 'Aug Actual',
                 field: 'actualAugust',
-                color: '#1b924d',
+                color: '#106d37',
             }]
         } else if (dsa.monthMode() == 'august') {
             series = [{
@@ -432,7 +432,7 @@ dsa.renderChartDailySalesAnalysis = function (data) {
             }, {
                 name: 'Aug Actual',
                 field: 'actualAugust',
-                color: '#1b924d',
+                color: '#106d37',
             }]
         }
 
